7658375198e33ce6b837b8cf582a76d8482bbdfb
hrasity <hrasity@hotmail.com>
2025-07-15 11:46:50 +0300

    new file:   .gitattributes 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/art/timestop/scheduler/SchedulerApplication.java 	new file:   src/main/java/art/timestop/scheduler/config/SchedulerConfig.java 	new file:   src/main/java/art/timestop/scheduler/controller/TaskController.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/art/timestop/scheduler/SchedulerApplicationTests.java

---
+++ src/main/java/art/timestop/scheduler/SchedulerApplication.java
@@ -0,0 +1,13 @@
+package art.timestop.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/art/timestop/scheduler/controller/TaskController.java
@@ -0,0 +1,54 @@
+package art.timestop.scheduler.controller;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.concurrent.ScheduledFuture;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Component
+@RequestMapping("/task")
+public class TaskController {
+    
+    @Autowired
+    private ThreadPoolTaskScheduler taskScheduler;
+
+    private ScheduledFuture<?> scheduledFuture;
+
+    long fixedRateMillis = 5000; // Every 5 seconds  
+    
+    private final Runnable taskRunnable = () -> {
+        System.out.println("Task Running: " + LocalDateTime.now());
+    };
+
+    @GetMapping("/start")
+    public String startTask() {
+        if (scheduledFuture != null && !scheduledFuture.isDone()) {
+            return "task is already running.";
+        }
+        
+        scheduledFuture = taskScheduler.scheduleAtFixedRate(taskRunnable, Instant.now(), Duration.ofMillis(fixedRateMillis));
+        return "Task Started...";
+    }
+    
+    @GetMapping("/stop")
+    public String stopTask() {
+        if (scheduledFuture == null || scheduledFuture.isDone()) {
+            return "Task is not running or already stopped.";
+        }
+        boolean cancelled = scheduledFuture.cancel(true);
+        if (cancelled) {
+            return "task stopped.";
+        } else {
+            return "Failed to stop task (might have just completed).";
+        }
+    }
+}
